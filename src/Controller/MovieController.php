3ac4a3c1be25f2a78c3bf7200ac2dee45eb8f0ac
Valentin Magry <valou32720@gmail.com>
2022-02-03 14:21:08 +0100

    Remove white space

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,8 +6,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\HttpFoundation\RequestStack;
+
 use App\TheMovieDB\TheMovieDbClient;
 
 
